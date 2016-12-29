7695fe28f5002145769b662b2d04694d914f4f16
LukaszKorycinski <lukasz.korycinski@gmail.com>
2016-12-29 19:49:39 +0100

    init

---
+++ java/com/kalkulator/lukasz/indexwig/AsyncResponse.java
@@ -0,0 +1,12 @@
+package com.kalkulator.lukasz.indexwig;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Lukasz on 2016-12-28.
+ */
+
+public interface AsyncResponse {
+    public void processFinish(List<Float> output);
+}
