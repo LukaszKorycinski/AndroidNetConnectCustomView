ae3686a0d04f9d4085abaa1b56c3dcf27a63ca65
LukaszKorycinski <lukasz.korycinski@gmail.com>
2016-12-29 20:52:56 +0100

    bug fix

--- java/com/kalkulator/lukasz/indexwig/Statistic.java
+++ java/com/kalkulator/lukasz/indexwig/Statistic.java
@@ -7,29 +7,6 @@ import java.util.List;
  */
 
 public class Statistic {
-    static Float  calMinimum(List<Float> input)//if list is empty return 0.0f
-    {
-        Float minimum=0.0f;
-        if(!input.isEmpty())
-        {
-            minimum=input.get(0);
-            for(Float f:input)
-                if(minimum>f)minimum=f;
-        }
-        return minimum;
-    }
-
-    static Float calMaximum(List<Float> input)//if list is empty return 0.0f
-    {
-        Float maximum=0.0f;
-        if(!input.isEmpty())
-        {
-            maximum=input.get(0);
-            for(Float f:input)
-                if(maximum<f)maximum=f;
-        }
-        return maximum;
-    }
 
     static Float calAverage(List<Float> input)//if list is empty return 0.0f
     {
