001d6b846f02a307f420516ecd629cea9431fc88
LukaszKorycinski <lukasz.korycinski@gmail.com>
2016-12-29 21:14:03 +0100

    init

---
+++ java/com/kalkulator/lukasz/indexwig/Constants.java
@@ -0,0 +1,9 @@
+package com.kalkulator.lukasz.indexwig;
+
+/**
+ * Created by Lukasz on 2016-12-29.
+ */
+
+public interface Constants {
+    String BASE_URL = "http://xml.wyborcza.biz/ArchivalProfileExportServlet.servlet?p5=WIG&p6=2016-06-28&p7=2016-12-28&p8=1&p3=TXT&type=INDEX";
+}
